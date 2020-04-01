2a8d87508a251e0f90700341be4e37326781801e
claudyne <claudyneuy98@gmail.com>
2020-04-02 04:10:08 +0800

    integrated new templates to all page except calendar + fixed log in form

--- mysite/mysite/main/views.py
+++ mysite/mysite/main/views.py
@@ -228,7 +228,9 @@ def login_request(request):
         form = AuthenticationForm(request=request, data=request.POST)
         print(form)
         if form.is_valid():
+            print("valid")
             username = form.cleaned_data.get('username')
+            print(username)
             password = form.cleaned_data.get('password')
             user = authenticate(username=username, password=password)
             if user is not None:
