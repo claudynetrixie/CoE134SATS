cb0263362122eaea0e4dc1eb894b0230731c2523
claudyne <claudyneuy98@gmail.com>
2020-04-03 23:45:14 +0800

    revert sidebar changes

--- mysite/mysite/main/static/js/main.js
+++ mysite/mysite/main/static/js/main.js
@@ -14,11 +14,7 @@
 
 	$('#sidebarCollapse').on('click', function () {
       $('#sidebar').toggleClass('active');
-	  // close dropdowns
-      $('.collapse.in').toggleClass('in');
-      // and also adjust aria-expanded attributes we use for the open/closed arrows
-      // in our CSS
-      $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+
   });
 
 })(jQuery);
